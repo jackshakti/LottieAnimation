e21ea6a36245e4d84488b8b83ba4aea21dda5917
Sakthivel M <magicshakti@gmail.com>
2023-11-22 23:21:59 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# Android_AnimasyonDeneme
+LottieFiles Animation 
+
+
+
+https://user-images.githubusercontent.com/119357010/208958245-e321aec5-b083-4e07-8037-e0f999c590f5.mp4
+
---
+++ app/src/main/java/com/ozzyozdil/deneme/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ozzyozdil.deneme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
